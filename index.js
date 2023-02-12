0c4f016725bab954848acfdcc82ad2c6a68a8dbd
codesanta <jathurshan@protonmail.com>
2023-02-13 00:22:28 +0530

    variable assigning

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+//var keyword use panninal ore name a niraiya thadava rewrite pannalam
 var a=100;
 console.log(a)
 var a=200;
 console.log(a)
+//let use paninal oruka assign panina variable name a again use panna elathu
+let x=10;
+console.log(x);
+//let x=20; => thirumpa assign panninal error varum
+
+//const same let seira athe work thaan seiyum
+let y=25;
+console.log(y);
+/*
+let y=30;
+console.log(y);
+*/
